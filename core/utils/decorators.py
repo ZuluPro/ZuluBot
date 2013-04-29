81ed37160fbcfdb52d6bf2a4837ffc2fb22b2de7
ZuluPro <montheanthony@hotmail.com>
2013-04-29 05:18:34 +0200

    Add settings.DEBUG option in ajax decorator; Add docs tp decorators

--- core/utils/decorators.py
+++ core/utils/decorators.py
@@ -1,6 +1,11 @@
 from django.http import Http404
+from django.conf import settings
 
 class method_restricted_to(object):
+    """
+    Restrict view to the given list of method.
+    Raise 404 if method isn't allowed.
+    """
     def __init__(self, methods=[]):
         self.methods = methods
 
@@ -14,11 +19,15 @@ class method_restricted_to(object):
         return wrapped_f
 
 class is_ajax(object):
+    """
+    Restrict view to ajax client.
+    Raise 404 if DEBUG = True
+    """
     def __call__(self, f):
         def wrapped_f(request, *args):
-            if request.is_ajax():
-                return f(request, *args)
-            else:
+            if not request.is_ajax() and not settings.DEBUG:
                 raise Http404
+            else:
+                return f(request, *args)
             
         return wrapped_f
